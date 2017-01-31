61ec5082edfeaf2b00060cccfebf3a672594e844
Eddie Kaleb <Eddie Kaleb>
2017-01-30 22:14:51 -0300

    first update

---
+++ app/src/main/java/com/example/eddie/musicplayer/MusicService.java
@@ -0,0 +1,179 @@
+package com.example.eddie.musicplayer;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.IBinder;
+import java.util.ArrayList;
+import java.util.Random;
+
+import android.content.ContentUris;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Binder;
+import android.os.PowerManager;
+import android.util.Log;
+
+/**
+ * Created by eddie on 29/01/2017.
+ */
+
+public class MusicService extends Service implements
+        MediaPlayer.OnPreparedListener, MediaPlayer.OnErrorListener,
+        MediaPlayer.OnCompletionListener {
+
+    //media player
+    private MediaPlayer player;
+    //song list
+    private ArrayList<Song> songs;
+    //current position
+    private int songPosn;
+    private final IBinder musicBind = new MusicBinder();
+    private boolean shuffle=false;
+    private Random rand;
+
+
+    @Override
+    public void onCreate() {
+        //create the service
+        super.onCreate();
+        //initialize position
+        songPosn=0;
+        //create player
+        player = new MediaPlayer();
+        initMusicPlayer();
+        rand=new Random();
+    }
+
+    public void initMusicPlayer(){
+        //set player properties
+        player.setWakeMode(getApplicationContext(),
+                PowerManager.PARTIAL_WAKE_LOCK);
+        player.setAudioStreamType(AudioManager.STREAM_MUSIC);
+
+        player.setOnPreparedListener(this);
+        player.setOnCompletionListener(this);
+        player.setOnErrorListener(this);
+
+    }
+
+    public void setList(ArrayList<Song> theSongs){
+        songs=theSongs;
+    }
+
+    public class MusicBinder extends Binder {
+        MusicService getService() {
+            return MusicService.this;
+        }
+    }
+
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return musicBind;
+    }
+
+    @Override
+    public boolean onUnbind(Intent intent){
+        player.stop();
+        player.release();
+        return false;
+    }
+
+    @Override
+    public void onCompletion(MediaPlayer mp) {
+        if(player.getCurrentPosition()>0){
+            mp.reset();
+            playNext();
+        }
+    }
+
+    @Override
+    public boolean onError(MediaPlayer mp, int what, int extra) {
+        mp.reset();
+        return false;
+    }
+
+    @Override
+    public void onPrepared(MediaPlayer mp) {
+        //start playback
+        mp.start();
+    }
+
+    public void setSong(int songIndex){
+        songPosn=songIndex;
+    }
+
+    public void setShuffle(){
+        if(shuffle) shuffle=false;
+        else shuffle=true;
+    }
+
+    public void playSong(){
+        //play a song
+        player.reset();
+        //get song
+        Song playSong = songs.get(songPosn);
+        //get id
+        long currSong = playSong.getID();
+        //set uri
+        Uri trackUri = ContentUris.withAppendedId(
+                android.provider.MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
+                currSong);
+
+        try{
+            player.setDataSource(getApplicationContext(), trackUri);
+        }
+        catch(Exception e){
+            Log.e("MUSIC SERVICE", "Error setting data source", e);
+        }
+
+        player.prepareAsync();
+    }
+
+    public int getPosn(){
+        return player.getCurrentPosition();
+    }
+
+    public int getDur(){
+        return player.getDuration();
+    }
+
+    public boolean isPng(){
+        return player.isPlaying();
+    }
+
+    public void pausePlayer(){
+        player.pause();
+    }
+
+    public void seek(int posn){
+        player.seekTo(posn);
+    }
+
+    public void go(){
+        player.start();
+    }
+
+    public void playPrev(){
+        songPosn--;
+        if(songPosn<0) songPosn=songs.size()-1;
+        playSong();
+    }
+
+    public void playNext(){
+        if(shuffle){
+            int newSong = songPosn;
+            while(newSong==songPosn){
+                newSong=rand.nextInt(songs.size());
+            }
+            songPosn=newSong;
+        }
+        else{
+            songPosn++;
+            if(songPosn>=songs.size()) songPosn=0;
+        }
+        playSong();
+    }
+}
